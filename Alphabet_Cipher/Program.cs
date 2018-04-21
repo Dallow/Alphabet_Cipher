eedbc65e5be6ea7b0000c2718a326009844b2d9d
dallow <simonrogiercarton@gmail.com>
2018-04-21 21:32:24 +0200

    Removed weird spaces.

--- Alphabet_Cipher/Program.cs
+++ Alphabet_Cipher/Program.cs
@@ -1,48 +1,48 @@
-﻿using System;
+	﻿using System;
 
-namespace Alphabet_Cipher
-{
-	class MainClass
+	namespace Alphabet_Cipher
 	{
-		public static void Main (string[] args)
+		class MainClass
 		{
-			if (args.Length < 3) {
-				Console.Write ("-- This program requires 3 arguments (mode, message and key) --");
-				return;
-			}
-			
-			string key = args [1];
-			string mes = args [2];
-
-			switch (args[0]) {
-			case "-e":
-				for (int i = 0; i < mes.Length; i++) {
-					Console.Write (EncryptChar (mes [i], key [i % key.Length]));
+			public static void Main (string[] args)
+			{
+				if (args.Length < 3) {
+					Console.Write("-- This program requires 3 arguments (mode, message and key) --");
+					return;
 				}
-				break;
-			case "-d":
-				for (int i = 0; i < mes.Length; i++) {
-					Console.Write (DecryptChar (mes [i], key [i % key.Length]));
+
+				string key = args[1];
+				string mes = args[2];
+
+				switch (args[0]) {
+				case "-e":
+					for (int i = 0; i < mes.Length; i++) {
+						Console.Write(EncryptChar(mes[i], key[i % key.Length]));
+					}
+					break;
+				case "-d":
+					for (int i = 0; i < mes.Length; i++) {
+						Console.Write(DecryptChar(mes[i], key[i % key.Length]));
+					}
+					break;
+				default:
+					Console.Write("-- No correct argument given. Type -e to encrypt and -d to decrypt --");
+					break;
 				}
-				break;
-			default:
-				Console.Write ("-- No correct argument given. Type -e to encrypt and -d to decrypt --");
-				break;
 			}
-		}
 
-		static char EncryptChar(char m, char k)
-		{
-			int c = (m + k - 12) % 26; 			// ('a' + 'a') % 26 = 12
-			return (char) (c + 'a');
-		}
+			static char EncryptChar(char m, char k)
+			{
+				int c = (m + k - 12) % 26; 			// ('a' + 'a') % 26 = 12
+				return (char) (c + 'a');
+			}
 
-		static char DecryptChar(char e, char k)
-		{
-			int i = e - k;
-			if (i < 0)
-				i += 26;
-			return (char)(i + 'a');
+			static char DecryptChar(char e, char k)
+			{
+				int i = e - k;
+				if (i < 0)
+					i += 26;
+				return (char)(i + 'a');
+			}
 		}
 	}
-}
