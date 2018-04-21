03753a54d23fb5e3a1d1166daf73968450a814aa
dallow <simonrogiercarton@gmail.com>
2018-04-21 21:47:32 +0200

    Removed tabs.

--- Alphabet_Cipher/Program.cs
+++ Alphabet_Cipher/Program.cs
@@ -1,48 +1,48 @@
-	﻿using System;
+﻿using System;
 
-	namespace Alphabet_Cipher
+namespace Alphabet_Cipher
+{
+	class MainClass
 	{
-		class MainClass
+		public static void Main (string[] args)
 		{
-			public static void Main (string[] args)
-			{
-				if (args.Length < 3) {
-					Console.Write("-- This program requires 3 arguments (mode, message and key) --");
-					return;
-				}
-
-				string key = args[1];
-				string mes = args[2];
-
-				switch (args[0]) {
-				case "-e":
-					for (int i = 0; i < mes.Length; i++) {
-						Console.Write(EncryptChar(mes[i], key[i % key.Length]));
-					}
-					break;
-				case "-d":
-					for (int i = 0; i < mes.Length; i++) {
-						Console.Write(DecryptChar(mes[i], key[i % key.Length]));
-					}
-					break;
-				default:
-					Console.Write("-- No correct argument given. Type -e to encrypt and -d to decrypt --");
-					break;
-				}
+			if (args.Length < 3) {
+				Console.Write("-- This program requires 3 arguments (mode, message and key) --");
+				return;
 			}
 
-			static char EncryptChar(char m, char k)
-			{
-				int c = (m + k - 12) % 26; 			// ('a' + 'a') % 26 = 12
-				return (char) (c + 'a');
-			}
+			string key = args[1];
+			string mes = args[2];
 
-			static char DecryptChar(char e, char k)
-			{
-				int i = e - k;
-				if (i < 0)
-					i += 26;
-				return (char)(i + 'a');
+			switch (args[0]) {
+			case "-e":
+				for (int i = 0; i < mes.Length; i++) {
+					Console.Write(EncryptChar(mes[i], key[i % key.Length]));
+				}
+				break;
+			case "-d":
+				for (int i = 0; i < mes.Length; i++) {
+					Console.Write(DecryptChar(mes[i], key[i % key.Length]));
+				}
+				break;
+			default:
+				Console.Write ("-- No correct argument given. Type -e to encrypt and -d to decrypt --");
+				break;
 			}
 		}
+
+		static char EncryptChar(char m, char k)
+		{
+			int c = (m + k - 12) % 26; 			// ('a' + 'a') % 26 = 12
+			return (char) (c + 'a');
+		}
+
+		static char DecryptChar(char e, char k)
+		{
+			int i = e - k;
+			if (i < 0)
+				i += 26;
+			return (char)(i + 'a');
+		}
 	}
+}
